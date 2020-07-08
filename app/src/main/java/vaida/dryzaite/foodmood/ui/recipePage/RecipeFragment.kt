7de8b05e893ba149db050df8db3f53f62b532a1e
Vaida Dryzaite <vaida@Vaidas-MacBook-Air.local>
2020-07-08 17:58:26 +0300

    favorite button, sharing via other apps - in detailpage

--- app/src/main/java/vaida/dryzaite/foodmood/ui/recipePage/RecipeFragment.kt
+++ app/src/main/java/vaida/dryzaite/foodmood/ui/recipePage/RecipeFragment.kt
@@ -1,18 +1,23 @@
 package vaida.dryzaite.foodmood.ui.recipePage
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.*
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.CheckBox
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.recipe_fragment.*
 import timber.log.Timber
 import vaida.dryzaite.foodmood.R
 import vaida.dryzaite.foodmood.databinding.RecipeFragmentBinding
+import vaida.dryzaite.foodmood.model.RecipeEntry
 import vaida.dryzaite.foodmood.utilities.convertNumericMealTypeToString
 
 
-class RecipeFragment : Fragment() {
+class RecipeFragment : Fragment(), Toolbar.OnMenuItemClickListener {
 
     private lateinit var recipeViewModel: RecipeViewModel
     private lateinit var binding: RecipeFragmentBinding
@@ -33,40 +38,38 @@ class RecipeFragment : Fragment() {
         return binding.root
         }
 
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        return  when (item?.itemId) {
+            R.id.menu_share_item -> {
+                shareRecipe()
+                true
+            }
+            else -> false
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //enable toolbar
-        (activity as AppCompatActivity).setSupportActionBar(binding.toolbarItem)
-        setHasOptionsMenu(true)
+        // inflate the toolbar menu with the heart, set up  observer, handling fav button toggle
+        toolbar_item.setOnMenuItemClickListener(this)
+        val favoriteMenuItem = toolbar_item.menu.findItem(R.id.favorite_item_selector)
+        val checkbox = favoriteMenuItem.actionView as CheckBox
+
+        recipeViewModel.recipeDetail.observe(viewLifecycleOwner, Observer {
+            setupFavoriteToggle(checkbox, it)
+        })
+
+        //observer handling clicks on URL field
+        recipeViewModel.navigateToUrl.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                Timber.i("$it is the URL to redirect")
+                redirectToRecipeUrl(it)
+                recipeViewModel.onButtonClicked()
+            }
+        })
+
     }
-
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.top_nav_menu_item, menu)
-
-        //setup clicks on icons in toolbar
-        val shareItem = menu.findItem(R.id.menu_share_item)
-        shareItem.actionView.setOnClickListener {
-            menu.performIdentifierAction(
-                shareItem.itemId,
-                0
-            )
-            Timber.i("menu item clicked")
-        }
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.menu_share_item -> {
-                Timber.i("executing Share Recipe intent")
-                shareRecipe()}
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-
 
 
 //    intent for sharing a recipe via other apps
@@ -78,11 +81,26 @@ class RecipeFragment : Fragment() {
         return shareIntent
     }
 
+
     private fun shareRecipe() {
         startActivity(getShareIntent())
     }
 
 
+    private fun setupFavoriteToggle(checkBox: CheckBox, recipe: RecipeEntry) {
+        checkBox.setOnCheckedChangeListener { _, boolean ->
+            recipe.isFavorite = boolean
+            recipeViewModel.updateRecipe(recipe)
+        }
+        checkBox.isChecked = recipe.isFavorite
+    }
+
+
+
+private fun redirectToRecipeUrl(url: String) {
+    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+    view?.context?.startActivity(intent)
+}
 
 
 }
--- app/src/main/java/vaida/dryzaite/foodmood/ui/recipePage/RecipeViewModel.kt
+++ app/src/main/java/vaida/dryzaite/foodmood/ui/recipePage/RecipeViewModel.kt
@@ -3,6 +3,8 @@ package vaida.dryzaite.foodmood.ui.recipePage
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import timber.log.Timber
 import vaida.dryzaite.foodmood.app.Injection
 import vaida.dryzaite.foodmood.model.RecipeEntry
 import vaida.dryzaite.foodmood.model.room.RecipeRepository
@@ -11,14 +13,33 @@ class RecipeViewModel (keyId: String, application: Application): AndroidViewMode
 
     private val repository: RecipeRepository = Injection.provideRecipeRepository(application)
 
-    private val detailRecipe: LiveData<RecipeEntry>
+    private val _detailRecipe: LiveData<RecipeEntry>
 
     init {
-        detailRecipe = repository.getRecipeWithId(keyId)
+        _detailRecipe = repository.getRecipeWithId(keyId)
     }
 
-    fun getDetailRecipe() = detailRecipe
+    fun getDetailRecipe() = _detailRecipe
 
     //for use of UI
     val recipeDetail = getDetailRecipe()
+
+    //updating database with status of favorites
+    fun updateRecipe(recipe: RecipeEntry) {
+        repository.updateRecipe(recipe)
+    }
+
+
+    // manage clicks on url link
+    private val _navigateToUrl = MutableLiveData<String?>()
+    val navigateToUrl: LiveData<String?>
+        get() = _navigateToUrl
+
+    fun onClickUrl() {
+        _navigateToUrl.value = recipeDetail.value?.recipe
+    }
+
+    fun onButtonClicked() {
+        _navigateToUrl.value = null
+    }
 }
